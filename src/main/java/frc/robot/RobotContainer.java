5af9dce4eecfc6275c561bf0edcb3b754a0233bf
Meatsquish <2007ogf@gmail.com>
2025-02-08 13:19:28 -0500

    Made a basic functioning motion

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,10 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -21,6 +23,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  private final ArmSubsystem arm = new ArmSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -48,7 +52,17 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    m_driverController.a().whileTrue(new InstantCommand(() -> arm.setJoint2Speed(0.05)));
+
+    m_driverController.y().whileTrue(new InstantCommand(() -> arm.setJoint2Speed(-0.05)));
+
+    m_driverController.rightBumper().whileTrue(new InstantCommand(() -> arm.setJoint1Speed(.05)));
+    m_driverController.leftBumper().whileTrue(new InstantCommand(() -> arm.setJoint1Speed(-0.05)));
+
+    m_driverController.b().onTrue(new InstantCommand(() -> arm.setJoint1Speed(0)));
+    m_driverController.x().onTrue(new InstantCommand(() -> arm.setJoint2Speed(0)));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,16 +4,56 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
-  
+  AnalogEncoder joint1Encoder;
+
+  AnalogEncoder joint2Encoder;
+
+  TalonFX joint1Motor;
+
+  TalonFX joint2Motor;
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    joint1Encoder = new AnalogEncoder(3);
+    // joint2Encoder = new AnalogEncoder(2);
+
+    joint1Motor = new TalonFX(21);
+    joint2Motor = new TalonFX(17);
+
+  }
+
+  public double getJointOnePosition()
+  {
+    return joint1Encoder.get();
+  }
+
+  // public double getJointTwoPosition()
+  // {
+  //   return joint2Encoder.get();
+  // }
+
+  public void setJoint1Speed(double speed)
+  {
+    joint1Motor.set(speed);
+  }
+
+  public void setJoint2Speed(double speed)
+  {
+    joint2Motor.set(speed);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    SmartDashboard.putNumber("Arm Joint 1 Position", getJointOnePosition());
+    // SmartDashboard.putNumber("Arm Joint 2 Position", getJointTwoPosition());
   }
 }
