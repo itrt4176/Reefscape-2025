5004c821ebdc6d8596d906afef982b0146f28d9c
SonnyCrem <snakeboss1999@example.com>
2025-02-10 16:38:50 -0500

    Added preliminary vision constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +19,9 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+  public static class VisionConstants {
+    //Get actual values
+    public static final Transform3d ROBOT_TO_CAM = new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0,0,0));
   }
 }
