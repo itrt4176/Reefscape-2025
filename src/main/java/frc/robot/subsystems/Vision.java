c8cecff457f08bc51d25346448b1ac6596dadfe9
SonnyCrem <snakeboss1999@example.com>
2025-02-10 16:39:40 -0500

    Started building vision subsystem

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,11 +4,34 @@
 
 package frc.robot.subsystems;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Vision extends SubsystemBase {
+
+  private PhotonCamera camera;
+  private PhotonPoseEstimator photonPoseEstimator;
+
+  //Is this the 2025 field?
+  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+
   /** Creates a new Vision. */
-  public Vision() {}
+  public Vision() {
+    
+    //Name the camera this
+    camera = new PhotonCamera("camera");
+    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,Constants.VisionConstants.ROBOT_TO_CAM);
+
+
+  }
 
   @Override
   public void periodic() {
