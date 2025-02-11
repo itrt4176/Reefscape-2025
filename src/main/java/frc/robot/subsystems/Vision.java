f6694668146d9d661703738b4e4540daa1f8c2cd
SonnyCrem <snakeboss1999@example.com>
2025-02-11 16:34:24 -0500

    Added 2025 field, getResult(), and rudimentary pose estimation that needs to probably be moved to Robot.java

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,11 +4,16 @@
 
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,22 +24,42 @@ public class Vision extends SubsystemBase {
   private PhotonCamera camera;
   private PhotonPoseEstimator photonPoseEstimator;
 
-  //Is this the 2025 field?
-  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+  private final static AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
+  private final static PoseStrategy poseStrategy = PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR;
 
   /** Creates a new Vision. */
   public Vision() {
     
-    //Name the camera this
+    //Name the camera
     camera = new PhotonCamera("camera");
-    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,Constants.VisionConstants.ROBOT_TO_CAM);
-
-
+    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,poseStrategy,Constants.VisionConstants.kRobotToCam);
   }
+
+  public Optional<PhotonPipelineResult> getResult() {
+    var results = camera.getAllUnreadResults();
+    PhotonPipelineResult result = null;
+    if (!results.isEmpty()) {
+        // Camera processed a new frame since last
+        // Get the last one in the list.
+        result = results.get(results.size() - 1);
+    }
+    return Optional.of(result);
+  }
+
+  // public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+  //       photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+  //       return photonPoseEstimator.update();
+  //   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    Optional<PhotonPipelineResult> optResult = getResult();
+    if (optResult.isPresent()) {
+      PhotonPipelineResult result = optResult.get();
+      photonPoseEstimator.update(result);
+    }
+
   }
 }
