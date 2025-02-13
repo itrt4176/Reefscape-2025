d3945ca497b53f502141c2dd69630dcc8ac3d9d7
Nick Manos <nmanos@eastonrobotics.org>
2025-02-13 01:30:11 -0500

    Add ArmCommands class with command factories for controlling both arm joints

---
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.ArmJoint;
+import frc.robot.subsystems.ArmJoint.Position;
+
+public class ArmCommands {
+  private final ArmJoint joint1;
+  private final ArmJoint joint2;
+  private final Trigger atGoal;
+
+  public ArmCommands(ArmJoint joint1, ArmJoint joint2) {
+    this.joint1 = joint1;
+    this.joint2 = joint2;
+    atGoal = joint1.atGoal().and(joint2.atGoal());
+  }
+
+  public Command setPosition(Position position) {
+    return joint1.setPosition(position)
+      .alongWith(joint2.setPosition(position));
+  }
+
+  public Command adjustOffset(DoubleSupplier joint1Adjuster, DoubleSupplier joint2Adjuster) {
+    return joint1.adjustOffset(joint1Adjuster)
+      .alongWith(joint2.adjustOffset(joint2Adjuster));
+  }
+
+  public Trigger atGoal() {
+    return atGoal;
+  }
+}
