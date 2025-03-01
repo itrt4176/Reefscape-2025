ff65ef0f8c7d86b94bc1d54497711cf46ccf0cf4
Puqi Luo <ptluo06@gmail.com>
2025-03-01 14:45:34 -0500

    adjusted more setpoints

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -115,7 +115,7 @@ public final class Constants {
     public static final Map<ArmJoint.Position, Double> angleMap = new EnumMap<>(ArmJoint.Position.class);
 
     static {
-      angleMap.put(ArmJoint.Position.CLIMB, 118.0);
+      angleMap.put(ArmJoint.Position.CLIMB, 123.0);
       angleMap.put(ArmJoint.Position.INTAKE, 197.8);
       angleMap.put(ArmJoint.Position.LEVEL_ONE, 2.5);
       angleMap.put(ArmJoint.Position.LEVEL_TWO, 28.3);
