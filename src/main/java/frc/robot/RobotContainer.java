929fc9e1af431c9217269b7b54614599d05a46be
Nick Manos <nmanos@eastonrobotics.org>
2025-03-14 00:22:14 -0400

    Clean up RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,74 +4,47 @@
 
 package frc.robot;
 
-import frc.robot.Constants.ShoulderJointConstants;
-import frc.robot.Constants.ClawConstants;
-import frc.robot.Constants.ElbowJointConstants;
-
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Meter;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import java.io.File;
 import java.util.Set;
-import java.util.function.DoubleSupplier;
 import java.util.function.Function;
-import java.util.function.Supplier;
-
-import com.ctre.phoenix.led.RainbowAnimation;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants.ClawConstants;
+import frc.robot.Constants.ElbowJointConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.PreferenceConstants;
-import frc.robot.commands.IntakePositioning;
-import frc.robot.subsystems.Intake;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.ArcingSpeed;
+import frc.robot.Constants.ShoulderJointConstants;
 import frc.robot.commands.ArmCommands;
 import frc.robot.commands.ClawSetArcAngle;
-import frc.robot.commands.CrazyShit;
 import frc.robot.commands.HomeWrist;
+import frc.robot.commands.IntakePositioning;
 import frc.robot.commands.RotationSetpoint;
-import frc.robot.commands.RotationSpeed;
+import frc.robot.subsystems.ArmJoint;
+import frc.robot.subsystems.ArmJoint.Position;
 import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.ArmJoint;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.ArmJoint.Position;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
-import swervelib.SwerveInputStream;
 import frc.robot.utils.BrakingMotors;
 import frc.robot.utils.CommandTigerPad;
 import frc.robot.utils.TigerPad.LEDMode;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -84,28 +57,12 @@ public class RobotContainer {
 
   private final Claw claw = new Claw();
 
-  private final ArcingSpeed arcing = new ArcingSpeed(claw, 0.05);
-
-  private final ArcingSpeed reverseArc = new ArcingSpeed(claw, -0.05);
-  private final RotationSpeed rotate = new RotationSpeed(claw, .3);
-
-  private final Command levelFour = new ClawSetArcAngle(claw, 50)
-      .andThen(new RotationSetpoint(claw, 0));
-
-  private final ClawSetArcAngle intakeClaw = new ClawSetArcAngle(claw, 50);
-
   // private final ClawSetArcAngle levelOneClaw = new ClawSetArcAngle(claw, 160);
 
   // private final ClawSetArcAngle levelTwoClaw = new ClawSetArcAngle(claw, 206.0);
 
 
   // private final ClawSetArcAngle levelThreeClaw = new ClawSetArcAngle(claw, 225.0);
-
-  private final RotationSetpoint ninetyRot = new RotationSetpoint(claw, 90);
-
-  private final CrazyShit test1 = new CrazyShit(claw, 90.0, claw.getArcDegrees());
-
-  private final CrazyShit test2 = new CrazyShit(claw, 0.0, claw.getArcDegrees());
 
 
   private final HomeWrist homeWrist = new HomeWrist(claw);
@@ -152,8 +109,6 @@ public class RobotContainer {
   );
 
   private final Intake intake = new Intake();
-  private final IntakePositioning storeIntake = new IntakePositioning(intake, IntakeConstants.STORE_ANGLE);
-  private final IntakePositioning intakeDown = new IntakePositioning(intake, IntakeConstants.INTAKE_DOWN);
   private final IntakePositioning intakeClimb = new IntakePositioning(intake, IntakeConstants.CLIMB);
 
   private final BrakingMotors[] brakingSubsystems = {drivebase, shoulderJoint, elbowJoint, claw};
@@ -334,14 +289,8 @@ public class RobotContainer {
       () -> intake.setSpeed(0) 
     ));
 
-    // driverController.x().onTrue(test1);
-    // driverController.y().onTrue(test2);
-
     driverController.leftBumper().whileTrue(new StartEndCommand(() -> climber.setWinchSpeed(1.0), () -> climber.setWinchSpeed(0), climber));
     driverController.rightBumper().whileTrue(new StartEndCommand(() -> climber.setWinchSpeed(-1.0), () -> climber.setWinchSpeed(0), climber));
-
-    // driverController.leftBumper().whileTrue(new StartEndCommand(() -> claw.setArcingSpeed(0.2), () -> claw.setArcingSpeed(0), claw));
-    // driverController.rightBumper().whileTrue(new StartEndCommand(() -> claw.setArcingSpeed(-0.2), () -> claw.setArcingSpeed(0), claw));
 
 
     driverController.leftTrigger(0.5).whileTrue(
@@ -486,7 +435,6 @@ public class RobotContainer {
       )
     );
 
-
     // claw.homed().onTrue(runOnce(claw::zeroRotation));
   }
 
@@ -548,27 +496,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    // return armCommands.setPosition(Position.LEVEL_ONE).andThen(
-    //   waitUntil(armCommands.atGoal()).withTimeout(1.5),
-    //   new HomeWrist(claw),
-    //   setWrist(ClawConstants.L1_ARC, ClawConstants.L1_ROT),
-    //   drivebase.driveToDistanceCommand(Units.inchesToMeters(70 - 10), -0.25)
-    // );
-
-    // return new HomeWrist(claw).andThen(
-    //   waitSeconds(.075),
-    //   setWristAndArm(
-    //     ClawConstants.L1_ARC,
-    //     ClawConstants.L1_ROT,
-    //     Position.LEVEL_ONE,
-    //     armControlPanel::setLevel1LED
-    //   ),
-    //   drivebase.driveToDistanceCommand(Units.inchesToMeters(70 - 10), -0.25)
-    // );
-
-    // return drivebase.driveToDistanceCommand(Units.inchesToMeters(60), -0.25);
-
     return autoChooser.getSelected();
   }
 }
